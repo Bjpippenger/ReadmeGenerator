df2b9b6861f458b463278369bfcea64fa203d19d
Bjpippenger <144498503+Bjpippenger@users.noreply.github.com>
2023-11-18 09:03:53 -0800

    updated code for index and generate markdown

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,46 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a description of your project:',
+  },
+  // Add more questions for other sections of the README
+  // Example:
+  // {
+  //   type: 'input',
+  //   name: 'installation',
+  //   message: 'Provide installation instructions:',
+  // },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('README file generated successfully!');
+    }
+  });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    const markdown = generateMarkdown(answers);
+    writeToFile('README.md', markdown);
+  });
+}
 
 // Function call to initialize app
 init();
